faa3e54a1a8ab4c21b5385979a72e0cd2cf5da7a
nk-rifat <nk.rifat76@gmail.com>
2025-07-13 08:06:52 +0600

    project setup with Vite, React, and Tailwind CSS

---
+++ src/data/data.js
@@ -0,0 +1,77 @@
+import TShirt from "../assets/img/image 1.png";
+import Polo from "../assets/img/image 10-1.png";
+import BlackStriped from "../assets/img/image 10-2.png";
+import Jeans from "../assets/img/image 7-1.png";
+import Checkered from "../assets/img/image 8-2.png";
+import SleeveStriped from "../assets/img/image 8.png";
+import VerticalStriped from "../assets/img/image 9-1.png";
+import Courage from "../assets/img/image 9-2.png";
+import Shorts from "../assets/img/image 9.png";
+
+const products = [
+  {
+    title: "Gradient Graphic T-shirt",
+    img: TShirt,
+    price: 145,
+    rating: 4,
+    stock: 212,
+  },
+  {
+    title: "Polo with Tipping Details",
+    img: Polo,
+    price: 180,
+    rating: 1,
+    stock: 320,
+  },
+  {
+    title: "Black Striped T-shirt",
+    img: BlackStriped,
+    price: 120,
+    rating: 3,
+    stock: 420,
+  },
+  {
+    title: "Skinny Fit Jeans",
+    img: Jeans,
+    price: 240,
+    rating: 4,
+    stock: 20,
+  },
+  {
+    title: "Checkered Shirt",
+    img: Checkered,
+    price: 180,
+    rating: 4,
+    stock: 20,
+  },
+  {
+    title: "Sleeve Striped T-shirt",
+    img: SleeveStriped,
+    price: 130,
+    rating: 4,
+    stock: 20,
+  },
+  {
+    title: "Vertical Striped Shirt",
+    img: VerticalStriped,
+    price: 212,
+    rating: 4,
+    stock: 20,
+  },
+  {
+    title: "Courage Graphic T-shirt",
+    img: Courage,
+    price: 145,
+    rating: 4,
+    stock: 20,
+  },
+  {
+    title: "Loose Fit Bermuda Shorts",
+    img: Shorts,
+    price: 80,
+    rating: 4.5,
+    stock: 10,
+  },
+];
+
+export default products;
